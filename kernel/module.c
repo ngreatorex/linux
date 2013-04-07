dc198aed8fd8eca6cd090d999a3a768c73d57a8b
Jan Kiszka <jan.kiszka@siemens.com>
2013-04-07 11:09:08 +0200

    module: Do not inline do_init_module
    This provides a reliable breakpoint target, required for automatic
    symbol loading via the gdb helper command 'lx-symbols'.
    
    CC: Rusty Russell <rusty@rustcorp.com.au>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- kernel/module.c
+++ kernel/module.c
@@ -3006,8 +3006,13 @@ static void do_mod_ctors(struct module *mod)
 #endif
 }
 
-/* This is where the real work happens */
-static int do_init_module(struct module *mod)
+/*
+ * This is where the real work happens.
+ *
+ * Keep it uninlined to provide a reliable breakpoint target, e.g. for the gdb
+ * helper command 'lx-symbols'.
+ */
+static noinline int do_init_module(struct module *mod)
 {
 	int ret = 0;
 
